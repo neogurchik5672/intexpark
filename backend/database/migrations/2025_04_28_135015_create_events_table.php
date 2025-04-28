a3b4ec3b55e482c6164890a02c1cec985c035e29
OgurechikxD <vdovichenko.ogur@mail.ru>
2025-04-28 20:03:09 +0600

    Create events

---
+++ backend/database/migrations/2025_04_28_135015_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('type');
+            $table->string('name');
+            $table->string('count')->default(0);
+            $table->string('subject');
+            $table->string('salary');
+            $table->string('data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
