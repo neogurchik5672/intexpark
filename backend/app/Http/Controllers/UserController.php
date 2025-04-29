fc84a632491756955306e38eda3341d69056f08b
RogLike <113023667+neogurchik5672@users.noreply.github.com>
2025-04-29 14:15:31 +0300

    my events

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Member;
 
 class UserController extends Controller
 {
@@ -11,8 +12,9 @@ class UserController extends Controller
     $query = User::query()->get();
     return view('user.index', compact('query'));
 }
-    public function show(){
+    public function show(){        
         $query = User::query()->first();
-        return view('user.show', compact('query'));
+        $myEvents = Member::query()->where('user_id',$query->id)->get();
+        return view('user.show', compact('query','myEvents'));
     }
 }
--- backend/resources/views/user/show.blade.php
+++ backend/resources/views/user/show.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.app')
 @section('content')
 <div class="show">
+    <div class="user">
     <h1> {{$query->tg_id}} тг юзернейм</h1>
-    <span>{{$query->balance}} коинов</span>
+    <span>{{$query->balance}} коинов</span> 
+    </div>
+    <div class="myEvents">
+        <h1>Ближайше события</h1>
+        @foreach ($myEvents as $item)
+        <div class="item">
+            <h1>{{$item->events->name}}</h1>
+            <p>{{$item->events->desc}}</p>    
+            <p>{{$item->events->type}}</p>
+            <p>{{$item->events->subject}}</p>
+            <p>{{$item->events->salary}} коинов</p>
+            @if($item->events->type == 'offline')
+            <p>{{$item->events->data}} {{$item->time}}</p>
+            <p>{{$item->events->count}} макс. участников</p>
+        </div>
+        @endif    
+    @endforeach
+    </div>
 </div>
 @endsection
