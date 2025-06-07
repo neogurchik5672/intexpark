a2b3b604aca6f02863296992ebab4d8c5b73a69a
oOUMUOo <chilikina71@mail.ru>
2025-06-08 02:36:47 +0600

    User  profile + editing

--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,11 @@ Route::get('/achievements', function () {
     $user = auth()->user(); // или получить пользователя как-то иначе
     return view('achievements.index', compact('achievements', 'user'));
 });
+//Страницы пользователя(+редактирование)
+Route::get('/user/account',[UserController::class,'account'])->name('user.account');
+Route::get('/user/user_view/{id}',[UserController::class,'user_view'])->name('user.user_view')->middleware('role:admin');
+Route::get('/user/user_editing/{id}',[UserController::class,'user_editing'])->name('user.user_editing')->middleware('role:admin');
+//Обновление данных пользователя (для админа)
+Route::post('/user/update-user-data', [UserController::class, 'updateUserData'])->name('user.update-user-data')->middleware('role:admin');
+//Удаление пользователя 
+Route::post('/user/delete-user', [UserController::class, 'deleteUser'])->name('user.delete-user')->middleware('role:admin');
