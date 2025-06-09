18ada1886b4292c59a3f2124db4ccd51f12314c9
OgurechikxD <vdovichenko.ogur@mail.ru>
2025-06-09 17:42:21 +0600

    Admin check

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\EmulateAuth::class, ///Для эмуляции авторизации (при ее отсутствии)
+        \App\Http\Middleware\CheckAdmin::class,
+        
     ];
 
     /**
@@ -45,6 +47,7 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        'admin' => \App\Http\Middleware\CheckAdmin::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -44,28 +44,18 @@ Route::get('/buy/index',[BuyRequestController::class,'index'])->name('buyRequest
 Route::get('/buy/show/{id}',[BuyRequestController::class,'show'])->name('buyRequest.show');
 Route::post('/buy/create/{id}',[BuyRequestController::class,'create'])->name('buyRequest.create');
 Route::get('/user/show',[UserController::class,'show'])->name('user.show');
-Route::get('/user/index',[UserController::class,'index'])->name('user.index');
-Route::get('/events/create',[EventsController::class,'create'])->name('events.create');
-Route::post('/events/store',[EventsController::class,'store'])->name('events.store');
 Route::get('/events/index',[EventsController::class,'index'])->name('events.index');
 Route::post('/member/store/{events}',[MemberController::class,'store'])->name('member.store');
-Route::get('/products/create',[ProductController::class,'create'])->name('product.create');
-Route::post('/products/store',[ProductController::class,'store'])->name('product.store');
 Route::post('/checkEvent/statusOff/{item}',[CheckEventController::class,'statusOff'])->name('checkEvent.statusOff');
 Route::post('/checkEvent/statusOffNot/{item}',[CheckEventController::class,'statusOffNot'])->name('checkEvent.statusOffNot');
-Route::get('/admin/index',[AdminController::class,'index'])->name('admin.index');
-Route::delete('/products/destroy/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('/products/update/{id}', [ProductController::class, 'update'])->name('products.update');
-Route::get('/admin/products',[AdminController::class,'products'])->name('admin.products');
 Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
 Route::get('/cart/index', [CartController::class, 'index'])->name('cart.index');
 Route::delete('/cart/destroy/{id}', [CartController::class, 'destroy'])->name('cart.destroy');
 Route::match(['PUT', 'GET'],'/user/updateCoins/{id}', [UserController::class, 'updateCoins'])->name('user.updateCoins');
 // Route::match(['PUT', 'GET'],'/user/updateCoin/{id}', [UserController::class, 'updateCoin'])->name('user.updateCoin');
 Route::get('/user/all/{id}',[UserController::class,'all'])->name('user.all');
-Route::get('/admin/transaction',[AdminController::class,'transaction'])->name('admin.transaction');
-Route::put('/admin/newAdmin/{id}',[AdminController::class,'newAdmin'])->name('admin.newAdmin');
+
+
 Route::post('/user/addAvatar/{$id}',[UserController::class,'all'])->name('user.addAvatar');
 // Админка
 Route::prefix('admin')->name('admin.')->group(function () {
@@ -87,3 +77,18 @@ Route::get('/user/user_editing/{id}',[UserController::class,'user_editing'])->na
 Route::post('/user/update-user-data', [UserController::class, 'updateUserData'])->name('user.update-user-data')->middleware('role:admin');
 //Удаление пользователя 
 Route::post('/user/delete-user', [UserController::class, 'deleteUser'])->name('user.delete-user')->middleware('role:admin');
+
+Route::group(['middleware' => ['auth', 'admin']], function() {
+Route::get('/admin/index',[AdminController::class,'index'])->name('admin.index');
+Route::get('/admin/products',[AdminController::class,'products'])->name('admin.products');
+Route::get('/admin/transaction',[AdminController::class,'transaction'])->name('admin.transaction');
+Route::put('/admin/newAdmin/{id}',[AdminController::class,'newAdmin'])->name('admin.newAdmin');
+Route::get('/events/create',[EventsController::class,'create'])->name('events.create');
+Route::post('/events/store',[EventsController::class,'store'])->name('events.store');
+Route::get('/user/index',[UserController::class,'index'])->name('user.index');
+Route::get('/products/create',[ProductController::class,'create'])->name('product.create');
+Route::post('/products/store',[ProductController::class,'store'])->name('product.store');
+Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/update/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/destroy/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+});
