11fe18bd55feb117ad623c1379f673a85121a0fc
rEvelina <95285606+rEvelina@users.noreply.github.com>
2025-06-09 12:14:59 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\TelegramLoginController;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\TelegramController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
