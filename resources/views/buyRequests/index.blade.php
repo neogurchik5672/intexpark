aa364c91e43b479f892c12c95f2936dbe4d28f2a
neogurchik5672 <zkrav375@gmail.com>
2025-06-10 18:44:36 +0300

    FIX

--- resources/views/buyRequests/index.blade.php
+++ resources/views/buyRequests/index.blade.php
@@ -15,7 +15,7 @@
           <div class="dropdown-content">
             @foreach ($item->buyRequest as $buyItem)
         <a href="{{ route('buyRequest.show',$buyItem->id) }}">
-          {{ $buyItem->user->telegram_id}}  
+          {{ $buyItem->user->username}}  
         </a>
         @endforeach
         @else
