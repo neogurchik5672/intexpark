e876e186a494c412dcc0a597ed0587dd96397b75
rEvelina <95285606+rEvelina@users.noreply.github.com>
2025-06-10 17:27:35 +0600

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\UpdateLastVisit::class,
-              \App\Http\Middleware\Header::class,
+         //     \App\Http\Middleware\Header::class,
         ],
 
         'api' => [
@@ -72,6 +72,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class, ///Для авторизации
-        'header' => \App\Http\Middleware\Header::class,
+      //  'header' => \App\Http\Middleware\Header::class,
     ];
 }
